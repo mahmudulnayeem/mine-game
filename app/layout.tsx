a83d456a4a7c10b7e38a27c8c3495529f84396f6
Mahmudul Hasan Nayeem <hasan@basikmarketing.in>
2024-06-20 11:48:33 +0600

    feat: added more metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,29 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Mine game",
   description: "Mine game made with React and TypeScript",
+  keywords: [
+    "minesweeper",
+    "game",
+    "react",
+    "typescript",
+    "next",
+    "mine game",
+    "minesweeper game",
+  ],
+  creator: "Mahmudul Hasan Nayeem",
+  category: "Game",
+  openGraph: {
+    type: "website",
+    phoneNumbers: ["+8801780064264"],
+    emails: ["hasan@basikmarketing.in", "mahmudulnayeem71@gmail.com"],
+    images: [{ url: "/preview.webp" }],
+    title: "Mine game",
+    description: "Find all the diamonds without stepping on a mine",
+    siteName: "Mine game",
+    url: "https://mine-diamond.vercel.app/",
+  },
+  icons:
+    '[{"src":"/favicon.ico","sizes":"64x64 32x32 24x24 16x16","type":"image/x-icon"}]',
 };
 
 export default function RootLayout({
